cc82a92d33a1a743b1529245eb8f238b19f96b93
Austin Melendez <AustinJames17@icloud.com>
2022-12-20 20:31:34 -0500

    edit about page and add new content from ben

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -3,12 +3,42 @@ import Link from "next/link";
 import Image from "next/image";
 
 import styles from "./About.module.scss";
-import { Button } from "../../components";
+import { Button, Logo } from "../../components";
 
 const About = () => {
   return (
     <div className={styles.about}>
       <div className={styles.mainContent}>
+        <div className={styles.content}>
+          <span className="tag">CPA, Founder, CEO</span>
+          <h1>Nalette Consulting</h1>
+          <div className={styles.tagline}>
+            Specializing in the not-for-profit industry, Nalette Consulting
+            prides itself on honesty and integrity. We are dedicated to
+            providing exceptional service to our clients and helping them
+            achieve their financial and operational goals. We have a wide range
+            of experience in all matters of accounting and taxation of nonprofit
+            organizations and are committed to assisting our clients to meet
+            their financial reporting and tax compliance requirements. Our staff
+            size is large enough to meet your needs, yet small enough to deliver
+            the responsive service you expect.
+          </div>
+        </div>
+        <div className={styles.logo}>
+          <Logo gradient />
+        </div>
+      </div>
+
+      <div className={styles.profile}>
+        <div className={styles.hero}>
+          <Image
+            src="https://res.cloudinary.com/austinmel/image/upload/v1671318417/naletteconsulting/Professional_Photo_1.jpg"
+            alt=""
+            width="601"
+            height="601"
+            layout="responsive"
+          />
+        </div>
         <div className={styles.content}>
           <span className={styles.tag}>CPA, Founder, CEO</span>
           <h1>Benjamin Nalette</h1>
@@ -22,20 +52,11 @@ const About = () => {
             <Button label="Keep reading" url="#aboutBen" secondary />
           </div>
         </div>
-        <div className={styles.hero}>
-          <Image
-            src="https://res.cloudinary.com/austinmel/image/upload/v1671318417/naletteconsulting/Professional_Photo_1.jpg"
-            alt=""
-            width="601"
-            height="601"
-            layout="responsive"
-          />
-        </div>
       </div>
 
       <div id="aboutBen" className={styles.bio}>
         <div className={styles.inner}>
-          <div className={styles.imageWrapper}>
+          {/* <div className={styles.imageWrapper}>
             <div className={styles.image}>
               <Image
                 src="https://res.cloudinary.com/austinmel/image/upload/v1671319748/naletteconsulting/ben-family.jpg"
@@ -43,7 +64,7 @@ const About = () => {
                 fill
               />
             </div>
-          </div>
+          </div> */}
           <div>
             <h2 className={styles.title}>More about Benjamin</h2>
             <div className={styles.content}>
@@ -55,15 +76,21 @@ const About = () => {
               his wife and daughter.
               <br />
               <br />
+              Professional Qualifications and Education
+              <ul>
+                <li>
+                  Certified Public Accountant (CPA), Commonwealth of Virginia
+                </li>
+                <li>
+                  Bachelors of Science in Public Accountancy and Finance from
+                  State University of New York at Fredonia.
+                </li>
+              </ul>
+              <br />
               <strong>Fun Fact:</strong> Ben loves ultrarunning and backpacking.
               He has attempted seven (7) 100 mile races, however has never run a
               marathon.
             </div>
-            {/* <div className={styles.funFact}>
-              <h3>fun fact</h3>
-              Ben loves ultrarunning and backpacking. He has attempted seven (7)
-              100 mile races, however has never run a marathon.
-            </div> */}
           </div>
         </div>
       </div>
