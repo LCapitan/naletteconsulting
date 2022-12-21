4eb9c5426c82dc419cc568f2de1977c5b15e78fd
Austin Melendez <AustinJames17@icloud.com>
2022-12-20 20:42:45 -0500

    add footer info

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,6 +16,11 @@ export function Footer() {
           <div className={styles.logo}>
             <Logo />
           </div>
+          <address>11166 Fairfax Blvd Ste 500 #1005 Fairfax, VA 22030</address>
+          <div className={styles.desktop}>(860) 707-3000</div>
+          <div className={styles.mobile}>
+            <a href="tel:8607073000">(860) 707-3000</a>
+          </div>
         </div>
       </div>
     </footer>
